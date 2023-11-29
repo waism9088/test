9898bd5e547647b15b2ffcd4b95a07ecaeb5232f
waism9088 <110425545+waism9088@users.noreply.github.com>
2023-11-29 22:24:24 +1100

    Create main.tf

---
+++ terraform_sn/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_region" "current_region" {}
+
+locals {
+  team        = "cbc"
+  sap_system  = "trbk"
+  server_name = "${var.environment}-ec2-sn"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.variables_ami
+  instance_type = var.variables_instance_size
+  subnet_id     = var.variables_subnet
+
+  tags = {
+    Name   = "${local.team}-${local.sap_system}-${local.server_name}"
+    Team   = "${local.team}-${local.sap_system}"
+    Region = data.aws_region.current_region.name
+    Resource = test-sn
+  }
+}
